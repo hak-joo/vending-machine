f8fec60150604e21e283c52e06de3e7dbafc0250
HyoinJeong <hiamy0107@naver.com>
2024-04-02 16:09:36 +0900

    Feat: 충전 페이지 함수 구조 설계

--- src/pages/charge/Charge.tsx
+++ src/pages/charge/Charge.tsx
@@ -1,32 +1,34 @@
+import React, { ChangeEvent } from 'react';
+import { useState } from 'react';
 import { useAtom } from 'jotai';
 import { userAtom } from '@/store/atoms';
 
 function Charge() {
   const [user, setUser] = useAtom(userAtom);
+  const [money, setMoney] = useState(0);
 
-  const chargeMoney = (money: number) => {
-    const updatedUser = {
-      ...user,
-      money: user.money + money,
-    };
-    setUser(updatedUser);
-  };
+  const chargeMoney = () => {};
+
+  const handleMoneyChange = (e: ChangeEvent<HTMLInputElement>) => {};
+
   return (
     <>
-      <button
-        className="border border-gray-500 mr-10"
-        onClick={() => chargeMoney(100)}
-      >
-        100
-      </button>
-      <button
-        className="border border-gray-500"
-        onClick={() => chargeMoney(500)}
-      >
-        500
+      <input
+        type="number"
+        id="money"
+        placeholder="금액"
+        onChange={handleMoneyChange}
+        style={{
+          display: 'block',
+          marginBottom: '10px',
+          border: '1px solid',
+        }}
+      />
+      <button className="border border-gray-500 mr-10" onClick={chargeMoney}>
+        충전
       </button>
 
-      <div>{user.money}</div>
+      <div>현재 잔액 : {user.money}</div>
     </>
   );
 }
