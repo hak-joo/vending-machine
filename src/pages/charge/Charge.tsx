ea8efa7a87113509eab70e996c00e62defc23c73
HyoinJeong <hiamy0107@naver.com>
2024-04-02 16:35:45 +0900

    Feat: 금액 입력 후 충전 기능 구현 #13

--- src/pages/charge/Charge.tsx
+++ src/pages/charge/Charge.tsx
@@ -6,10 +6,26 @@ import { userAtom } from '@/store/atoms';
 function Charge() {
   const [user, setUser] = useAtom(userAtom);
   const [money, setMoney] = useState(0);
+  const [errorMassage, setErrorMessage] = useState('');
 
-  const chargeMoney = () => {};
+  const chargeMoney = () => {
+    if (!confirm('충전 하시겠습니까?')) {
+      return;
+    }
 
-  const handleMoneyChange = (e: ChangeEvent<HTMLInputElement>) => {};
+    const updatedUser = {
+      ...user,
+      money: user.money + money,
+    };
+    setUser(updatedUser);
+    setMoney(0);
+  };
+
+  const handleMoneyChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputMoney = e.target.value;
+    const replaceMoney = inputMoney.replace(/(^0+)/, '');
+    setMoney(parseInt(replaceMoney));
+  };
 
   return (
     <>
@@ -17,6 +33,7 @@ function Charge() {
         type="number"
         id="money"
         placeholder="금액"
+        value={money === 0 ? '' : money}
         onChange={handleMoneyChange}
         style={{
           display: 'block',
@@ -24,6 +41,7 @@ function Charge() {
           border: '1px solid',
         }}
       />
+      <p style={{ color: 'red' }}>{errorMassage}</p>
       <button className="border border-gray-500 mr-10" onClick={chargeMoney}>
         충전
       </button>
