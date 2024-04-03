b03e1f223da25188d4f40f8a3c0e5139cb6b2772
HyoinJeong <hiamy0107@naver.com>
2024-04-03 13:28:12 +0900

    Feat: 사용자 충전 UI에  tailwindcss 적용 #14

--- src/components/charge/ChargeMoneyBtnItem.tsx
+++ src/components/charge/ChargeMoneyBtnItem.tsx
@@ -3,7 +3,7 @@ type ChargeMoneyBtnItemProps = {
 };
 
 function ChargeMoneyBtnItem({ money }: ChargeMoneyBtnItemProps) {
-  return <button className="border border-gray-500 mr-10">{money}</button>;
+  return <button className="border border-gray-500 mr-10">+{money}</button>;
 }
 
 export default ChargeMoneyBtnItem;
--- src/pages/charge/Charge.tsx
+++ src/pages/charge/Charge.tsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 import { useAtom } from 'jotai';
 import { userAtom } from '@/store/atoms';
 import ChargeMoneyBtnList from '@/components/charge/ChargeMoneyBtnList';
+import '@/styles/charge/Charge.css';
 
 function Charge() {
   const [user, setUser] = useAtom(userAtom);
   const [money, setMoney] = useState(0);
-  const [errorMassage, setErrorMessage] = useState('');
 
   const chargeMoney = () => {
     if (!confirm('충전 하시겠습니까?')) {
@@ -29,29 +29,29 @@ function Charge() {
   };
 
   return (
-    <>
-      <input
-        type="number"
-        id="money"
-        placeholder="금액"
-        value={money === 0 ? '' : money}
-        onChange={handleMoneyChange}
-        style={{
-          display: 'block',
-          marginBottom: '10px',
-          border: '1px solid',
-        }}
-      />
-      <div>
-        <ChargeMoneyBtnList />
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="w-1/3 h-1/4 flex flex-col items-center border border-indigo-500/50 rounded-lg">
+        <p className="text-lg text-cyan-600 mt-10 mb-2">사용자 금액 충전</p>
+        <input
+          type="number"
+          id="money"
+          className="w-56 border border-gray-500 rounded-lg m-2 pr-2 text-right block  appearance-none"
+          placeholder="금액"
+          value={money === 0 ? '' : money}
+          onChange={handleMoneyChange}
+        />
+        <div className="my-4 mx-16">
+          <ChargeMoneyBtnList />
+        </div>
+        <button
+          className="w-14 h-10 border border-indigo-500/50 rounded-lg  bg-indigo-500/50 text-white m-2"
+          onClick={chargeMoney}
+        >
+          충전
+        </button>
+        <p className="font-bold m-2">현재 잔액 : {user.money}</p>
       </div>
-      <p style={{ color: 'red' }}>{errorMassage}</p>
-      <button className="border border-gray-500 mr-10" onClick={chargeMoney}>
-        충전
-      </button>
-
-      <div>현재 잔액 : {user.money}</div>
-    </>
+    </div>
   );
 }
 
