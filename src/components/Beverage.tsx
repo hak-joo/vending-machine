ae586664d8ef5e4be7adf534941d700e0fefaca4
dongsuu <suudh99@naver.com>
2024-04-04 16:45:54 +0900

    feat: add beverage component

---
+++ src/components/Beverage.tsx
@@ -0,0 +1,21 @@
+import { BeverageType } from '@/types/BeverageType';
+
+function Beverage({ beverage }: { beverage: BeverageType }): JSX.Element {
+  return (
+    <div>
+      <div className="bg-white flex justify-center items-center flex-col">
+        <div className="">
+          <img src={beverage.imagePath} className="" />
+        </div>
+        <div className="">
+          <h2>{beverage.name}</h2>
+        </div>
+        <div className="">
+          <div>{beverage.price}원</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Beverage;
