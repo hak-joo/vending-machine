5d5606c2c16d347216b0392c95e784fd255d7af3
HyoinJeong <hiamy0107@naver.com>
2024-04-05 10:27:06 +0900

    Fix: 사용자 충전 금액 데이터 분리 #14

--- src/components/charge/ChargeMoneyBtnList.tsx
+++ src/components/charge/ChargeMoneyBtnList.tsx
@@ -1,52 +1,5 @@
 import CommonBtn from '@/components/button/CommonBtn';
-import { ButtonProps } from '@/types/Button';
-
-// 충전 금액 type
-// 공통 버튼 컴포넌트의 props에 money 속성 추가
-type chargeMoneyType = ButtonProps & {
-  money: number;
-};
-
-// 충전 금액 리스트
-// 가격, className이 정의
-const chargeMoneyList: chargeMoneyType[] = [
-  {
-    money: 100,
-    bgColor: 'gray',
-    textColor: 'white',
-    shape: 'circle',
-    border: 'gray',
-    textAlign: 'right',
-    additionalClass: 'w-14 h-14 mr-5',
-  },
-  {
-    money: 500,
-    bgColor: 'gray',
-    textColor: 'white',
-    shape: 'circle',
-    border: 'gray',
-    textAlign: 'right',
-    additionalClass: 'w-14 h-14 mr-5',
-  },
-  {
-    money: 1000,
-    bgColor: 'blue',
-    textColor: 'white',
-    shape: 'rectangle',
-    border: 'blue',
-    textAlign: 'right',
-    additionalClass: 'w-20 h-14 mr-5',
-  },
-  {
-    money: 5000,
-    bgColor: 'blue',
-    textColor: 'white',
-    shape: 'rectangle',
-    border: 'blue',
-    textAlign: 'right',
-    additionalClass: 'w-20 h-14 mr-5',
-  },
-];
+import { chargeMoneyList } from './data';
 
 type ChargeMoneyBtnListType = {
   onIncreaseMoney: (increaseMoney: number) => void;
