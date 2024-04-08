86adadf255a6cc915d1cf8f993fe04be974144f9
dongsuu <suudh99@naver.com>
2024-04-08 12:24:11 +0900

    feat: add inputMoney context api

--- src/context/InputMoneyContext.tsx
+++ src/context/InputMoneyContext.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-export const inputMoneyContext = React.createContext({});
+export interface InputMoneyContextType {
+  inputMoney: number;
+  setInputMoney: (money: number) => void;
+}
+
+export const useInputMoney = () => {
+  const [inputMoney, setInputMoney] = React.useState<number>(0);
+
+  const handleClick = (money: number) => {
+    setInputMoney(inputMoney + money);
+    console.log(inputMoney);
+  };
+
+  return {
+    inputMoney,
+    handleClick,
+  };
+};
+
+export const inputMoneyContext = React.createContext<InputMoneyContextType>({
+  inputMoney: 0,
+  setInputMoney: () => {},
+});
 
 export const InputMoney = ({ children }: { children: React.ReactNode }) => {
-  const [inputMoney, setInputMoney] = React.useState<number>(0);
+  const { inputMoney, setInputMoney } =
+    React.useContext<InputMoneyContextType>(inputMoneyContext);
 
   return (
     <inputMoneyContext.Provider value={{ inputMoney, setInputMoney }}>
--- src/pages/VendingMachine/VendingMachine.tsx
+++ src/pages/VendingMachine/VendingMachine.tsx
@@ -1,14 +1,13 @@
 import Beverage from '@/pages/VendingMachine/Beverage/Beverage';
 import { BeverageType } from './Beverage/types';
 import BeverageAPI from '@/util/BeverageApi';
-import React, { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ChargeMoneyBtnList from '../Charge/ChargeMoneyBtnList/ChargeMoneyBtnList';
-import { inputMoneyContext } from '@/context/InputMoneyContext';
+import { InputMoney, useInputMoney } from '@/context/InputMoneyContext';
 
 function VendingMachinePage() {
   const [beverageList, setBeverageList] = useState<BeverageType[]>([]);
-  const [inputMoney, setInputMoney] = useState<number>(0);
-  // const { inputMoeny } = useContext(inputMoneyContext);
+  const { inputMoney, handleClick } = useInputMoney();
 
   const fetch = () => {
     const beverageList = BeverageAPI.fetchBeverageList();
@@ -19,42 +18,44 @@ function VendingMachinePage() {
     fetch();
   }, []);
 
-  const onIncreaseMoney = (moeny: number) => {
-    setInputMoney(inputMoney + moeny);
+  const handleIncreaseMoney = (money: number) => {
+    handleClick(money);
   };
 
   return (
-    <div className="flex flex-col p-64">
-      <div className="basis-1/3 bg-lime-300">
-        <div className="grid grid-cols-6 gap-2 p-2 justify-items-center">
-          {beverageList.map((beverage) => (
-            <Beverage
-              key={beverage.id}
-              beverage={beverage}
-              inputMoney={inputMoney}
-            />
-          ))}
+    <InputMoney>
+      <div className="flex flex-col p-64">
+        <div className="basis-1/3 bg-lime-300">
+          <div className="grid grid-cols-6 gap-2 p-2 justify-items-center">
+            {beverageList.map((beverage) => (
+              <Beverage
+                key={beverage.id}
+                beverage={beverage}
+                inputMoney={inputMoney}
+              />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="w-full h-1 bg-black"></div>
-      <div className="basis-1/3">
-        <div className="flex flex-col bg-lime-300 p-2">
-          <div className="">
-            <div className="text-lime-700 text-4xl font-bold bg-lime-300 border-4 border-lime-700 rounded-lg p-2 text-right">
-              넣은 돈: {inputMoney}
+        <div className="w-full h-1 bg-black"></div>
+        <div className="basis-1/3">
+          <div className="flex flex-col bg-lime-300 p-2">
+            <div className="">
+              <div className="text-lime-700 text-4xl font-bold bg-lime-300 border-4 border-lime-700 rounded-lg p-2 text-right">
+                넣은 돈: {inputMoney}
+              </div>
+            </div>
+            <div className="flex flex-row p-2 justify-end">
+              <ChargeMoneyBtnList onIncreaseMoney={handleIncreaseMoney} />
             </div>
           </div>
-          <div className="flex flex-row p-2 justify-end">
-            <ChargeMoneyBtnList onIncreaseMoney={onIncreaseMoney} />
-          </div>
+        </div>
+
+        <div className="basis-1/3">
+          <button>음료 나오는 곳</button>
         </div>
       </div>
-
-      <div className="basis-1/3">
-        <button>음료 나오는 곳</button>
-      </div>
-    </div>
+    </InputMoney>
   );
 }
 
