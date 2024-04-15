f3d34c68217f6a38a0514e381babdd684dad86f8
HyoinJeong <hiamy0107@naver.com>
2024-04-15 09:08:10 +0900

    Style: 버튼 이미지 변수명 변경 #38

--- src/components/MoneyBtnList/types.ts
+++ src/components/MoneyBtnList/types.ts
@@ -4,5 +4,5 @@ import { CommandBtnType } from '@/components/CommanBtn/types';
 // 공통 버튼 컴포넌트의 props에 money 속성 추가
 export type ChargeMoneyType = CommandBtnType & {
   money: number;
-  imgUrl: string;
+  imgurl: string;
 };
